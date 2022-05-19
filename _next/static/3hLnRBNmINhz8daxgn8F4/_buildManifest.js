aed05d3af031944bcd989292f17b7b2452f14199
azaek <aloxel@gmail.com>
2022-05-19 14:36:05 +0530

    reacted

--- _next/static/3hLnRBNmINhz8daxgn8F4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5b6b99c2c9b962e8.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
